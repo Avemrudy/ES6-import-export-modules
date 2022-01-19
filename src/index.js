c10dc091414a696555ca453484ba18da3d04beab
Avemrudy <97418419+Avemrudy@users.noreply.github.com>
2022-01-19 03:47:03 +0000

    -

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+// import PI, { doublePi, triplePi, TriplePi } from "./math";
+import * as pi from "./math";
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    {/* <li>{PI}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li> */}
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
